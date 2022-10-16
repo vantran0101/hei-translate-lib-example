f508c9b5ba954844d4c78402d077772943db782d
st-vantran <van.tran@supremtech.vn>
2022-10-17 00:37:54 +0700

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { RoomClient } from '@heitech/voice-library-npm';
+
+const listDevices = async () => {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    console.log("enumerateDevices() not supported.");
+    return {};
+  }
+
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+
+    const devicesByKind: Record<string, Array<any>> = {};
+
+    devices.forEach((device) => {
+      const deviceInfo = {
+        id: device.deviceId,
+        label: device.label,
+        kind: device.kind,
+      };
+
+      if (!devicesByKind[device.kind]) {
+        devicesByKind[device.kind] = [];
+      }
+      devicesByKind[device.kind].push(deviceInfo);
+    });
+
+    return devicesByKind;
+  } catch (err) {
+    console.log(err);
+    return {};
+  }
+}
+
+// list devices
+const devices = listDevices().then(data => {
+  console.log(data);
+})
+
+// Enable mic (if you're in a meeting, do not need to do that)
+navigator.mediaDevices.getUserMedia({ audio: true });
+
+let voiceRecognition: RoomClient | null;
+
 function App() {
+  const [originalVoice, setOriginalVoice] = useState<string>('')
+  const [translatedVoice, setTranslatedVoice] = useState<string>('')
+
+  const callbackonTextRecognizing = (e: string) => {
+    setTranslatedVoice('');
+    setOriginalVoice(e);
+  }
+
+  const callbackOnTextRecognized = (e: string) => {
+    setTranslatedVoice(e)
+  }
+
+  useEffect(() => {
+    voiceRecognition = new RoomClient(
+      (textRecognizing: string) => callbackonTextRecognizing(textRecognizing),
+      (textRecognized: string) => callbackOnTextRecognized(textRecognized)
+    );
+
+    voiceRecognition.initTranslation(YOUR_KEY, 'en-US', 'vi-VN');
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +75,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Original Voice: </div>
+        <p>{originalVoice}</p>
+        <div>Translated Voice: </div>
+        <p>{translatedVoice}</p>
+        <h3 onClick={() => voiceRecognition?.close()}>TURN LISTENER OFF</h3>
+        <h3 onClick={() => voiceRecognition?.open()}>TURN LISTENER ON</h3>
       </header>
     </div>
   );
