564c7fb41b4f6fdf37990a789352985d509263d4
st-vantran <van.tran@supremtech.vn>
2022-10-26 22:56:19 +0700

    Update select language

--- src/App.tsx
+++ src/App.tsx
@@ -45,35 +45,29 @@ navigator.mediaDevices.getUserMedia({ audio: true });
 
 let voiceRecognition: RoomClient | null;
 
-window.HTMLElement.prototype.scrollIntoView = function () { };
-
 function App() {
   const [originalVoice, setOriginalVoice] = useState<string>('')
   const [translatedVoice, setTranslatedVoice] = useState<string>('')
-  const [isTexting, setIsTexting] = useState<boolean>(false)
   const [isListening, setIsListening] = useState<boolean>(true);
-  const [fullText, setFullText] = useState<string>('')
+  const [fullText, setFullText] = useState<string>('');
+  const [originalLang, setOriginalLang] = useState<string>('en-Us');
 
   const scrollRef = useRef<null | HTMLDivElement>(null);
 
   const callbackonTextRecognizing = (e: string) => {
-    setIsTexting(true)
     setOriginalVoice(e);
-
   }
 
   const callbackOnTextRecognized = (e: string) => {
-    setIsTexting(false)
+    console.log('Translated : ', e);
     setTranslatedVoice(e);
   }
 
   useEffect(() => {
     if (translatedVoice) {
-      const temp = fullText + `<div style="color: #61dafb">Original Voice:  <span>${originalVoice}</span></div>`
-      const fulltext2 = temp + ` <div style="color: #61dafb">Translated Voice:  <span>${translatedVoice}</span></div><p></p>`
-      console.log(fulltext2);
+      const temp = fullText + `<div style="color: #61dafb">Original:  <span>${originalVoice}</span></div>`
+      const fulltext2 = temp + ` <div style="color: #61dafb">Translated:  <span>${translatedVoice}</span></div><p></p>`
       setFullText(fulltext2);
-      // scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
       window.scrollTo(0, (scrollRef?.current?.offsetTop || 0) - 300);
     }
   }, [translatedVoice])
@@ -84,36 +78,70 @@ function App() {
       (textRecognized: string) => callbackOnTextRecognized(textRecognized)
     );
 
-    voiceRecognition.initTranslation('9a5ce5f10a64d86a0eb9e15be562249', 'en-US', 'vi-VN');
-
+    voiceRecognition.initTranslation('9a5ce5f10a64d86a0eb9e15be562249', 'en-Us', 'vi-VN');
   }, [])
+
+  const [originalLanguage, setOriginalLanguage] = useState(["en-US", "vi-VN", "fr-FR", "ja-JP"])
+  const [translatedLanguage, setTranslatedLanguage] = useState(["vi-VN", "en-US", "fr-FR", "ja-JP"])
+  const Add = originalLanguage.map(Add => Add)
+  const Add2 = translatedLanguage.map(Add => Add)
+  const [showLanguage, setShowLanguage] = useState('en-US')
+
+  const handleOriginalLanguage = (e: any) => {
+    console.log((originalLanguage[e.target.value]));
+    voiceRecognition?.changeLanguage(originalLanguage[e.target.value]);
+    setShowLanguage(originalLanguage[e.target.value])
+  }
+
+  const handleTranslateLanguage = (e: any) => {
+    console.log((translatedLanguage[e.target.value]));
+    voiceRecognition?.changeTranslateLanguage(translatedLanguage[e.target.value]);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
+
         <div className="info">
           <img src={'https://uploads-ssl.webflow.com/62a9b06138aac5c00fbb5f5e/62ad49be82ada255cc95750b_HEI%20LOGO.svg'} className="App-logo" alt="logo" />
           <p>
             Heitech Voice Translate Example
           </p>
 
-          {isListening && <button onClick={() => {
+          {isListening && <button className="btn btn-primary" onClick={() => {
             voiceRecognition?.close();
             setIsListening(false)
           }}>TURN LISTENER OFF</button>}
 
-          {!isListening && <button onClick={() => {
+          {!isListening && <button className="btn btn-primary" onClick={() => {
             voiceRecognition?.open();
             setIsListening(true)
           }}>TURN LISTENER ON</button>}
+        </div>
+        <div className="translateLanguage">
+          <label>Original Languages: </label>
+          < select
+            onChange={e => handleOriginalLanguage(e)}
+            className="form-select" >
+            {
+              Add.map((language, key) => <option value={key}>{language}</option>)
+            }
+          </select >
+          <label>Translate Languages: </label>
+          < select
+            onChange={e => handleTranslateLanguage(e)}
+            className="form-select" >
+            {
+              Add2.map((language, key) => <option value={key}>{language}</option>)
+            }
+          </select >
         </div>
         <div className="content">
           <div
             dangerouslySetInnerHTML={{ __html: fullText }}
           />
           <div className="realtime" ref={scrollRef} id="scroll" >
-            <div style={{ color: "#61dafb" }}>Original Voice:  <span>{originalVoice}</span></div>
-            <div style={{ color: "#61dafb" }}>Translated Voice:  <span>{isTexting ? '' : translatedVoice}</span></div>
+            <div style={{ color: "#61dafb" }}>Original Voice ({showLanguage}):  <span>{originalVoice}</span></div>
           </div>
         </div>
       </header>
